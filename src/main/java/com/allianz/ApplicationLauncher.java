9243a87ca0ae405a4127c27e904bfd72c2f7eb47
lohiam <mohit.lohia04@gmail.com>
2018-07-03 20:23:31 +0530

    this is updated version 1.0

--- src/main/java/com/allianz/ApplicationLauncher.java
+++ src/main/java/com/allianz/ApplicationLauncher.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJQueryAjaxClientApplication {
+public class ApplicationLauncher {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJQueryAjaxClientApplication.class, args);
+		SpringApplication.run(ApplicationLauncher.class, args);
 	}
 }
